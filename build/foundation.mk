31d6c58b913307b7d66421b449c2173de0885391
Qixiang Xu <qixiang.xu@arm.com>
2019-04-04 15:21:02 +0800

    add optee for foundation

--- build/foundation.mk
+++ build/foundation.mk
@@ -1,4 +1,5 @@
 CROSS_COMPILE ?= aarch64-linux-gnu- 
+CROSS_COMPILE32 ?= arm-linux-gnueabi-
 MODEL ?= /usr/local/DS-5_v5.29.1/sw/models/bin/Foundation_Platform
 TARGETS = u-boot arm-tf linux busybox ramdisk 
 
@@ -27,6 +28,43 @@ u-boot.build:
 u-boot.clean:
 	rm u-boot/out -rf 
 
+
+
+optee.build: 
+	export CROSS_COMPILE=$(CROSS_COMPILE) ; \
+	export CROSS_COMPILE64=$(CROSS_COMPILE) ; \
+	export CROSS_COMPILE32=$(CROSS_COMPILE32) ; \
+	export CFG_TEE_CORE_LOG_LEVEL=1 ; \
+	export CFG_ARM64_core=y ; \
+    cd optee/optee_os; \
+	make -j 16  PLATFORM=vexpress-fvp CFG_ARM_GICV3=y; \
+	cd ../.. ; \
+	cd optee/optee_client ; \
+	make -j 16 ; 
+
+optee.clean:
+	rm optee/optee_os/out -rf 
+	rm optee/optee_client/out -rf 
+
+
+arm-tf.optee.build:
+	export CROSS_COMPILE=$(CROSS_COMPILE) ; \
+	cd arm-tf; \
+	make -j 16  PLAT=fvp ARCH=aarch64 DEBUG=1  \
+	ARM_TSP_RAM_LOCATION=tdram NEED_BL32=yes SPD=opteed \
+	BL32=../optee/optee_os/out/arm-plat-vexpress/core/tee-header_v2.bin \
+    BL32_EXTRA1=../optee/optee_os/out/arm-plat-vexpress/core/tee-pager_v2.bin \
+    BL32_EXTRA2=../optee/optee_os/out/arm-plat-vexpress/core/tee-pageable_v2.bin \
+	BL33=../u-boot/out/u-boot.bin \
+	TRUSTED_BOARD_BOOT=1 ARM_ROTPK_LOCATION=devel_rsa \
+	KEY_ALG=rsa TF_MBEDTLS_KEY_ALG=rsa \
+	ROT_KEY=./plat/arm/board/common/rotpk/arm_rotprivk_rsa.pem  \
+	MBEDTLS_DIR=../mbedtls GENERATE_COT=1 \
+	FVP_HW_CONFIG_DTS=fdts/fvp-foundation-gicv3-psci.dts \
+	FVP_USE_GIC_DRIVER=FVP_GICV3 \
+	dtbs all fip;
+
+
 arm-tf.build: 
 	export CROSS_COMPILE=$(CROSS_COMPILE) ; \
 	cd arm-tf; \
@@ -36,6 +74,7 @@ arm-tf.build:
 	KEY_ALG=rsa TF_MBEDTLS_KEY_ALG=rsa \
 	ROT_KEY=./plat/arm/board/common/rotpk/arm_rotprivk_rsa.pem  \
 	MBEDTLS_DIR=../mbedtls GENERATE_COT=1 \
+	FVP_HW_CONFIG_DTS=fdts/fvp-foundation-gicv3-psci.dts \
 	dtbs all fip;
 
 arm-tf.clean:
@@ -80,10 +119,12 @@ ramdisk.clean:
 
 
 run:
-	$(MODEL) --cores=1 --no-secure-memory --visualization --use-real-time --gicv3  \
+	$(MODEL) --arm-v8.0 \
+	--cores=4 \
+	--secure-memory \
+	--visualization \
+	--gicv3 \
 	--data=arm-tf/build/fvp/debug/bl1.bin@0x0 \
 	--data=arm-tf/build/fvp/debug/fip.bin@0x8000000 \
 	--data=linux/out/arch/arm64/boot/Image@0x80080000 \
-	--data=linux/out/arch/arm64/boot/dts/arm/foundation-v8-gicv3.dtb@0x82000000 \
 	--data=ramdisk/ramdisk.img@0x84000000 \
-	--arm-v8.0
